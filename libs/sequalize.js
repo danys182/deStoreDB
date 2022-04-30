ed2849701e91046d4cb55ae197c61baa7d2104d2
Dany Schmidt <dnl1802@gmail.com>
2022-04-30 17:51:50 -0300

    Add config db

--- libs/sequalize.js
+++ libs/sequalize.js
@@ -4,34 +4,31 @@ const logger = require('./winston.createLogger');
 
 const setUpModels = require('../db/models/index');
 
-const USER = encodeURIComponent(config.dbUser);
-const PASSWORD = encodeURIComponent(config.dbPassword);
-
-const sequelize = new Sequelize(
-	`mysql://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`,
-	{
-		dialect: 'mysql',
-		logging: (msg) => logger.debug(msg),
-		hooks: {
-			beforeFind: (instance, options) => {
-				if (!instance.attributes) {
-					instance.attributes = {};
+const sequelize = new Sequelize(config.dbUrl, {
+	dialect: 'mysql',
+	logging: (msg) => logger.debug(msg),
+	ssl: {
+		rejectUnauthorized: false,
+	},
+	hooks: {
+		beforeFind: (instance, options) => {
+			if (!instance.attributes) {
+				instance.attributes = {};
+			}
+			instance.attributes.exclude = ['createdAt', 'updateAt'];
+			instance.include?.map((attr) => {
+				if (!attr.attributes) {
+					attr.attributes = {};
 				}
-				instance.attributes.exclude = ['createdAt', 'updateAt'];
-				instance.include?.map((attr) => {
-					if (!attr.attributes) {
-						attr.attributes = {};
-					}
-					return (attr.attributes.exclude = [
-						'createdAt',
-						'updateAt',
-						'password',
-					]);
-				});
-			},
+				return (attr.attributes.exclude = [
+					'createdAt',
+					'updateAt',
+					'password',
+				]);
+			});
 		},
-	}
-);
+	},
+});
 
 setUpModels(sequelize);
 
