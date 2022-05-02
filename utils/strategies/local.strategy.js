6db166ce597ce1178a8a3601184c4bcc521c26a4
Dany Schmidt <dnl1802@gmail.com>
2022-05-02 12:16:20 -0300

    fix require boom

--- utils/strategies/local.strategy.js
+++ utils/strategies/local.strategy.js
@@ -1,17 +1,17 @@
 const { Strategy } = require('passport-local');
-const { boom } = require('boom');
+const boom = require('@hapi/boom');
 const userService = require('./../../services/user.service');
 
-const LocalStrategy = new Strategy((username, email, done) => {
-    try { 
-        const user = await userService.findByEmail(email);
-        if (!user) { 
-            done(boom.unauthorized(),false);
-        }
-        done(null,user);
-    } catch (error) {
-        done(error, false);
-    }
+const LocalStrategy = new Strategy(async (username, email, done) => {
+	try {
+		const user = await userService.findByEmail(email);
+		if (!user) {
+			done(boom.unauthorized(), false);
+		}
+		done(null, user);
+	} catch (error) {
+		done(error, false);
+	}
 });
 
 module.exports = LocalStrategy;
