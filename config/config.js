89a91498b4f911b52ceb2a1833f835faa09b1679
Dany Schmidt <dnl1802@gmail.com>
2022-05-03 18:51:32 -0300

    Add env email

--- config/config.js
+++ config/config.js
@@ -7,6 +7,8 @@ const config = {
 	dbUrl: process.env.DATABASE_URL,
 	apiKey: process.env.API_KEY,
 	jwtSecret: process.env.JWT_SECRET,
+	emailUser: process.env.EMAIL_USER,
+	emailPassword: process.env.EMAIL_PASSWORD,
 };
 
 module.exports = { config };
